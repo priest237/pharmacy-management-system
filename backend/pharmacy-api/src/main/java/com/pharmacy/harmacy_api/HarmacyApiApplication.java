b718089a8133d7b34580627853b53a4a3758c839
priest237 <esambegeorge88@gmail.com>
2026-02-27 12:34:22 +0100

    Initial project structure with Spring Boot setup

---
+++ backend/pharmacy-api/src/main/java/com/pharmacy/harmacy_api/HarmacyApiApplication.java
@@ -0,0 +1,13 @@
+package com.pharmacy.harmacy_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmacyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmacyApiApplication.class, args);
+	}
+
+}
